fee45224a63a6f99e5e95fe98085fcda9ce97e9c
mlf167937-svg <mlf167937@gmail.com>
2026-06-14 10:06:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,18 @@ import os
 
 app = Flask(__name__)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+BASE = os.path.dirname(os.path.abspath(__file__))
+STATIC = os.path.join(BASE, "static")
 
-
-def read_txt(filename):
+def read_txt(name):
     try:
-        with open(os.path.join(STATIC_DIR, filename), "r", encoding="utf-8") as f:
+        with open(os.path.join(STATIC, name), "r", encoding="utf-8") as f:
             return f.read()
     except:
-        return "[text belum tersedia]"
-
+        return ""
 
 @app.route("/")
 def home():
-
     data = [
         {
             "img": "history1.png",
@@ -30,10 +27,7 @@ def home():
             "text": read_txt("history2.txt")
         }
     ]
-
     return render_template("index.html", data=data)
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
